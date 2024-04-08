373fb391385b42cfb121acafc0abf55f7849ff27
Alza Syauqi <syauqialza7@gmail.com>
2024-04-08 13:38:45 +0700

    translate domain error to HTTP error

--- src/infrastructures/http/_test/create-server.test.js
+++ src/infrastructures/http/_test/create-server.test.js
@@ -12,6 +12,20 @@ describe('HTTP server', () => {
     await UsersTableTestHelper.cleanTable()
   })
 
+  it('should response 404 when request unregistered route', async () => {
+    // arrange
+    const server = await createServer({})
+
+    // action
+    const response = await server.inject({
+      method: 'GET',
+      url: '/unregisteredRoute'
+    })
+
+    // assert
+    expect(response.statusCode).toEqual(404)
+  })
+
   describe('when POST /users', () => {
     it('should response 201 and persisted user', async () => {
       // arrange
@@ -34,6 +48,144 @@ describe('HTTP server', () => {
       expect(response.statusCode).toEqual(201)
       expect(responseJson.status).toEqual('success')
       expect(responseJson.data.addedUser).toBeDefined()
+    })
+
+    it('should response 400 when request payload not contain needed property', async () => {
+      // arrange
+      const requestPayload = {
+        fullname: 'Alza Syauqi',
+        password: 'secret'
+      }
+      const server = await createServer(container)
+
+      // action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/users',
+        payload: requestPayload
+      })
+
+      // assert
+      const responseJson = JSON.parse(response.payload)
+      expect(response.statusCode).toEqual(400)
+      expect(responseJson.status).toEqual('fail')
+      expect(responseJson.message).toEqual('tidak dapat membuat user baru karena properti yang dibutuhkan tidak ada.')
+    })
+
+    it('should response 400 when request payload not meet data type specification', async () => {
+      // arrange
+      const requestPayload = {
+        username: 'alzasyauqi',
+        password: 'secret',
+        fullname: ['Alza Syauqi']
+      }
+      const server = await createServer(container)
+
+      // action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/users',
+        payload: requestPayload
+      })
+
+      // assert
+      const responseJson = JSON.parse(response.payload)
+      expect(response.statusCode).toEqual(400)
+      expect(responseJson.status).toEqual('fail')
+      expect(responseJson.message).toEqual('tidak dapat membuat user baru karena tipe data tidak sesuai.')
+    })
+
+    it('should response 400 when username more than 50 character', async () => {
+      // arrange
+      const requestPayload = {
+        username: 'alzasyauqialzasyauqialzasyauqialzasyauqialzasyauqialzasyauqi',
+        password: 'secret',
+        fullname: 'Alza Syauqi'
+      }
+      const server = await createServer(container)
+
+      // action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/users',
+        payload: requestPayload
+      })
+
+      // assert
+      const responseJson = JSON.parse(response.payload)
+      expect(response.statusCode).toEqual(400)
+      expect(responseJson.status).toEqual('fail')
+      expect(responseJson.message).toEqual('tidak dapat membuat user baru karena karakter username melebihi batas limit.')
+    })
+
+    it('should response 400 when username contain restricted character', async () => {
+      // arrange
+      const requestPayload = {
+        username: 'alza syauqi',
+        password: 'secret',
+        fullname: 'Alza Syauqi'
+      }
+      const server = await createServer(container)
+
+      // action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/users',
+        payload: requestPayload
+      })
+
+      // assert
+      const responseJson = JSON.parse(response.payload)
+      expect(response.statusCode).toEqual(400)
+      expect(responseJson.status).toEqual('fail')
+      expect(responseJson.message).toEqual('tidak dapat membuat user baru karena username mengandung karakter terlarang.')
+    })
+
+    it('should response 400 when username unavailable', async () => {
+      // arrange
+      await UsersTableTestHelper.addUser({ username: 'alzasyauqi' })
+      const requestPayload = {
+        username: 'alzasyauqi',
+        password: 'secret',
+        fullname: 'Alza Syauqi'
+      }
+      const server = await createServer(container)
+
+      // action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/users',
+        payload: requestPayload
+      })
+
+      // assert
+      const responseJson = JSON.parse(response.payload)
+      expect(response.statusCode).toEqual(400)
+      expect(responseJson.status).toEqual('fail')
+      expect(responseJson.message).toEqual('username tidak tersedia.')
+    })
+
+    it('should handle server error correctly', async () => {
+      // arrange
+      const requestPayload = {
+        username: 'alzasyauqi',
+        password: 'super_secret',
+        fullname: 'Alza Syauqi'
+      }
+      const server = await createServer({}) // fake container
+
+      // action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/users',
+        payload: requestPayload
+      })
+
+      // assert
+      const responseJson = JSON.parse(response.payload)
+      expect(response.statusCode).toEqual(500)
+      expect(responseJson.status).toEqual('error')
+      expect(responseJson.message).toEqual('terjadi kegagalan pada server kami.')
     })
   })
 })
--- src/infrastructures/http/create-server.js
+++ src/infrastructures/http/create-server.js
@@ -1,4 +1,6 @@
 const Hapi = require('@hapi/hapi')
+const ClientError = require('../../commons/exceptions/client-error')
+const DomainErrorTranslator = require('../../commons/exceptions/domain-error-translator')
 const users = require('../../interfaces/http/api/users')
 const config = require('../../commons/config')
 
@@ -15,6 +17,42 @@ const createServer = async container => {
       options: { container }
     }
   ])
+
+  server.ext('onPreResponse', (request, h) => {
+    // get response context from request
+    const { response } = request
+
+    if (response instanceof Error) {
+      // if the response is error, handle as needed
+      const translatedError = DomainErrorTranslator.translate(response)
+
+      // handle client error internally
+      if (translatedError instanceof ClientError) {
+        const newResponse = h.response({
+          status: 'fail',
+          message: translatedError.message
+        })
+        newResponse.code(translatedError.statusCode)
+        return newResponse
+      }
+
+      // retains the client error handling by hapi natively, such as 404, etc.
+      if (!translatedError.isServer) {
+        return h.continue
+      }
+
+      // handle server error as needed
+      const newResponse = h.response({
+        status: 'error',
+        message: 'terjadi kegagalan pada server kami.'
+      })
+      newResponse.code(500)
+      return newResponse
+    }
+
+    // if not error, continue the previous response (without intervented)
+    return h.continue
+  })
 
   return server
 }
