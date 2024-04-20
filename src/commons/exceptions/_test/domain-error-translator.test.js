2f5915d10313bc070d7c8f49ba859c54d622abe3
Alza Syauqi <syauqialza7@gmail.com>
2024-04-21 00:17:59 +0700

    update domain-error-translation

--- src/commons/exceptions/_test/domain-error-translator.test.js
+++ src/commons/exceptions/_test/domain-error-translator.test.js
@@ -11,6 +11,21 @@ describe('DomainErrorTranslator', () => {
       .toStrictEqual(new InvariantError('tidak dapat membuat user baru karena karakter username melebihi batas limit.'))
     expect(DomainErrorTranslator.translate(new Error('REGISTER_USER.USERNAME_CONTAIN_RESTRICTED_CHARACTER')))
       .toStrictEqual(new InvariantError('tidak dapat membuat user baru karena username mengandung karakter terlarang.'))
+
+    expect(DomainErrorTranslator.translate(new Error('REGISTERED_USER.NOT_CONTAIN_NEEDED_PROPERTY')))
+      .toStrictEqual(new InvariantError('tidak dapat menampilkan user baru karena properti yang dibutuhkan tidak ada.'))
+    expect(DomainErrorTranslator.translate(new Error('REGISTERED_USER.NOT_MEET_DATA_TYPE_SPECIFICATION')))
+      .toStrictEqual(new InvariantError('tidak dapat menampilkan user baru karena tipe data tidak sesuai.'))
+
+    expect(DomainErrorTranslator.translate(new Error('NEW_AUTHENTICATION.NOT_CONTAIN_NEEDED_PROPERTY')))
+      .toStrictEqual(new InvariantError('tidak dapat membuat authentikasi baru karena properti yang dibutuhkan tidak ada.'))
+    expect(DomainErrorTranslator.translate(new Error('NEW_AUTHENTICATION.NOT_MEET_DATA_TYPE_SPECIFICATION')))
+      .toStrictEqual(new InvariantError('tidak dapat membuat authentikasi baru karena tipe data tidak sesuai.'))
+
+    expect(DomainErrorTranslator.translate(new Error('LOGIN_USER.NOT_CONTAIN_NEEDED_PROPERTY')))
+      .toStrictEqual(new InvariantError('tidak dapat login user karena properti yang dibutuhkan tidak ada.'))
+    expect(DomainErrorTranslator.translate(new Error('LOGIN_USER.NOT_MEET_DATA_TYPE_SPECIFICATION')))
+      .toStrictEqual(new InvariantError('tidak dapat login user karena tipe data tidak sesuai.'))
   })
 
   it('should return original error when error message is not needed to translate', () => {
--- src/commons/exceptions/domain-error-translator.js
+++ src/commons/exceptions/domain-error-translator.js
@@ -10,7 +10,13 @@ DomainErrorTranslator._directories = {
   'REGISTER_USER.NOT_CONTAIN_NEEDED_PROPERTY': new InvariantError('tidak dapat membuat user baru karena properti yang dibutuhkan tidak ada.'),
   'REGISTER_USER.NOT_MEET_DATA_TYPE_SPECIFICATION': new InvariantError('tidak dapat membuat user baru karena tipe data tidak sesuai.'),
   'REGISTER_USER.USERNAME_LIMIT_CHAR': new InvariantError('tidak dapat membuat user baru karena karakter username melebihi batas limit.'),
-  'REGISTER_USER.USERNAME_CONTAIN_RESTRICTED_CHARACTER': new InvariantError('tidak dapat membuat user baru karena username mengandung karakter terlarang.')
+  'REGISTER_USER.USERNAME_CONTAIN_RESTRICTED_CHARACTER': new InvariantError('tidak dapat membuat user baru karena username mengandung karakter terlarang.'),
+  'REGISTERED_USER.NOT_CONTAIN_NEEDED_PROPERTY': new InvariantError('tidak dapat menampilkan user baru karena properti yang dibutuhkan tidak ada.'),
+  'REGISTERED_USER.NOT_MEET_DATA_TYPE_SPECIFICATION': new InvariantError('tidak dapat menampilkan user baru karena tipe data tidak sesuai.'),
+  'NEW_AUTHENTICATION.NOT_CONTAIN_NEEDED_PROPERTY': new InvariantError('tidak dapat membuat authentikasi baru karena properti yang dibutuhkan tidak ada.'),
+  'NEW_AUTHENTICATION.NOT_MEET_DATA_TYPE_SPECIFICATION': new InvariantError('tidak dapat membuat authentikasi baru karena tipe data tidak sesuai.'),
+  'LOGIN_USER.NOT_CONTAIN_NEEDED_PROPERTY': new InvariantError('tidak dapat login user karena properti yang dibutuhkan tidak ada.'),
+  'LOGIN_USER.NOT_MEET_DATA_TYPE_SPECIFICATION': new InvariantError('tidak dapat login user karena tipe data tidak sesuai.')
 }
 
 module.exports = DomainErrorTranslator
