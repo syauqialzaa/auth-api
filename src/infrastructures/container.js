08fd2d0db0d76d9a29ccf4303397086d47bc930f
Alza Syauqi <syauqialza7@gmail.com>
2024-04-24 23:47:14 +0700

    update service locator

--- src/infrastructures/container.js
+++ src/infrastructures/container.js
@@ -4,16 +4,24 @@ const { createContainer } = require('instances-container')
 // external agency
 const { nanoid } = require('nanoid')
 const bcrypt = require('bcrypt')
+const Jwt = require('@hapi/jwt')
 const pool = require('./database/postgres/pool')
 
 // service (repository, helper, manager, etc.)
 const UserRepositoryPostgres = require('./repository/user-repository-postgres')
+const AuthenticationRepositoryPostgres = require('./repository/authentication-repository-postgres')
 const BcryptPasswordHash = require('./security/bcrypt-password-hash')
+const JwtTokenManager = require('./security/jwt-token-manager')
 
 // use case
 const AddUserUseCase = require('../applications/use_case/add-user-use-case')
+const LoginUserUseCase = require('../applications/use_case/login-user-use-case')
+const LogoutUserUseCase = require('../applications/use_case/logout-user-use-case')
+const RefreshAuthenticationUseCase = require('../applications/use_case/refresh-authentication-use-case')
 const UserRepository = require('../domains/users/user-repository')
+const AuthenticationRepository = require('../domains/authentications/authentication-repository')
 const PasswordHash = require('../applications/security/password-hash')
+const AuthenticationTokenManager = require('../applications/security/authentication-token-manager')
 
 // creating container
 const container = createContainer()
@@ -31,11 +39,29 @@ container.register([
     }
   },
   {
+    key: AuthenticationRepository.name,
+    Class: AuthenticationRepositoryPostgres,
+    parameter: {
+      dependencies: [
+        { concrete: pool }
+      ]
+    }
+  },
+  {
     key: PasswordHash.name,
     Class: BcryptPasswordHash,
     parameter: {
       dependencies: [
         { concrete: bcrypt }
+      ]
+    }
+  },
+  {
+    key: AuthenticationTokenManager.name,
+    Class: JwtTokenManager,
+    parameter: {
+      dependencies: [
+        { concrete: Jwt.token }
       ]
     }
   }
@@ -56,6 +82,61 @@ container.register([
         {
           name: 'passwordHash',
           internal: PasswordHash.name
+        }
+      ]
+    }
+  },
+  {
+    key: LoginUserUseCase.name,
+    Class: LoginUserUseCase,
+    parameter: {
+      injectType: 'destructuring',
+      dependencies: [
+        {
+          name: 'userRepository',
+          internal: UserRepository.name
+        },
+        {
+          name: 'authenticationRepository',
+          internal: AuthenticationRepository.name
+        },
+        {
+          name: 'authenticationTokenManager',
+          internal: AuthenticationTokenManager.name
+        },
+        {
+          name: 'passwordHash',
+          internal: PasswordHash.name
+        }
+      ]
+    }
+  },
+  {
+    key: LogoutUserUseCase.name,
+    Class: LogoutUserUseCase,
+    parameter: {
+      injectType: 'destructuring',
+      dependencies: [
+        {
+          name: 'authenticationRepository',
+          internal: AuthenticationRepository.name
+        }
+      ]
+    }
+  },
+  {
+    key: RefreshAuthenticationUseCase.name,
+    Class: RefreshAuthenticationUseCase,
+    parameter: {
+      injectType: 'destructuring',
+      dependencies: [
+        {
+          name: 'authenticationRepository',
+          internal: AuthenticationRepository.name
+        },
+        {
+          name: 'authenticationTokenManager',
+          internal: AuthenticationTokenManager.name
         }
       ]
     }
