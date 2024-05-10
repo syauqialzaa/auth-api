6cd200f33f80a84bcfac1f5807578e1d1f561212
Alza Syauqi <syauqialza7@gmail.com>
2024-05-10 08:21:41 +0700

    add say hello world test case

--- src/infrastructures/http/_test/create-server.test.js
+++ src/infrastructures/http/_test/create-server.test.js
@@ -29,6 +29,21 @@ describe('HTTP server', () => {
     expect(response.statusCode).toEqual(404)
   })
 
+  describe('when GET /', () => {
+    it('should return 200 and hello world', async () => {
+      const server = await createServer({})
+
+      const response = await server.inject({
+        method: 'GET',
+        url: '/'
+      })
+
+      const responseJson = JSON.parse(response.payload)
+      expect(response.statusCode).toEqual(200)
+      expect(responseJson.value).toEqual('Hello World!')
+    })
+  })
+
   describe('when POST /users', () => {
     it('should response 201 and persisted user', async () => {
       // arrange
